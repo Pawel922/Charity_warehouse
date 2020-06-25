728ea19e904eeca9ae2aa9228b3ae6cd21116671
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-06-25 22:33:52 +0200

    All institutions which are kept in database are displayed on the homepage

--- src/main/java/pl/coderslab/charity/controller/HomeController.java
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -2,14 +2,30 @@ package pl.coderslab.charity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.Institution;
+import pl.coderslab.charity.repository.InstitutionRepository;
+
+import java.util.List;
 
 
 @Controller
 public class HomeController {
 
+    private final InstitutionRepository institutionRepository;
+
+    public HomeController(InstitutionRepository institutionRepository) {
+        this.institutionRepository = institutionRepository;
+    }
+
     @RequestMapping("/")
     public String homeAction(Model model){
         return "index";
+    }
+
+    @ModelAttribute("institutions")
+    public List<Institution> getAllInstitutions() {
+        return institutionRepository.findAll();
     }
 }
