78147928351b260cdd017c8900598e7fd03de920
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-06-26 20:40:27 +0200

    All categories are displayed during filling the form

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -1,14 +1,29 @@
 package pl.coderslab.charity.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.Category;
+import pl.coderslab.charity.repository.CategoryRepository;
+
+import java.util.List;
 
 @Controller
 public class DonationController {
 
+    private final CategoryRepository categoryRepository;
+
+    public DonationController (CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
     @RequestMapping("/donation")
-    public String displayForm(Model model) {
+    public String displayForm() {
         return "form";
+    }
+
+    @ModelAttribute("categories")
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
     }
 }
