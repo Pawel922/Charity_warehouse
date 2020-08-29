7fc69c106a72c1e923b920af38647516e8e6a14b
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-08-29 23:04:16 +0200

    Method's name was changed

--- src/main/java/pl/coderslab/charity/repository/UserRepository.java
+++ src/main/java/pl/coderslab/charity/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.coderslab.charity.entity.User;
 
 public interface UserRepository extends JpaRepository <User, Long> {
-	User findByEmail(String email);
+	User findByName(String username);
 }
