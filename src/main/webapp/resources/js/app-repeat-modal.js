83657e08d549a553fd001d7eeb42946453cba0c3
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2021-02-10 20:25:21 +0100

    Email input is checked whether is correct

--- src/main/webapp/resources/js/app-repeat-modal.js
+++ src/main/webapp/resources/js/app-repeat-modal.js
@@ -7,17 +7,43 @@ document.addEventListener("DOMContentLoaded", function() {
 	// Get the <span> element that closes the modal
 	let span = document.getElementsByClassName("close")[0];
 	//Get input with email
-	let emailInput = document.querySelectorAll(".modal-body input");
+	let emailInput = document.querySelector(".modal-body input");
+	//Get submit button
+	let submitBtn = document.querySelector(".modal-body button")
+	// Get paragraph with warnings
+	let warning = document.querySelector(".modal-body .warning")
 	
 	
 	// When the user clicks on the button, open the modal to repeat password
 	repeatBtn.addEventListener("click", function(e){
 		e.preventDefault();
+		warning.innerText = "";
 		modal.style.display = "block";
 	})
 	
 	// When the user clicks on <span> (x), close the modal
 	span.addEventListener("click", function(){
 		modal.style.display = "none";
+	})
+	
+	// When the user clicks on submit button, check value of email input
+	submitBtn.addEventListener("click", function(e){
+		e.preventDefault();
+		
+		const regexToCheckEmpty = /^\s*$/;
+		const regexToCheckEmail = /[^0-9-_\.](\S)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.([a-zA-Z]{2,}){1}/;
+		let isValueCorrect = true;
+		
+		if(regexToCheckEmpty.test(emailInput.value)) {
+			isValueCorrect = false;
+			warning.innerText = "Nie może być puste";
+		} else if (!regexToCheckEmail.test(emailInput.value)) {
+			isValueCorrect = false;
+			warning.innerText = "Nieprawidłowy email";
+		}
+		
+		if(isValueCorrect) {
+			
+		}
 	})
 })
